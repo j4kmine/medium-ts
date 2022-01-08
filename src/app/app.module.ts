f2eae196cb00167bd511489a80321ae11f94415e
bayu <cbzvdani@gmail.com>
2022-01-08 15:01:00 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
+import {StoreModule} from '@ngrx/store'
+import {StoreDevtoolsModule} from '@ngrx/store-devtools'
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module'
+import {AppComponent} from './app.component'
+import {AuthModule} from 'src/app/auth/auth.module'
+import {environment} from 'src/environments/environment'
+import {EffectsModule} from '@ngrx/effects'
+import {TopBarModule} from 'src/app/shared/modules/topBar/topBar.module'
+import {PersistanceService} from './shared/services/persistance.service'
+import {AuthInterceptor} from './shared/services/authinterceptor.service'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    AuthModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production
+    }),
+    EffectsModule.forRoot([]),
+    TopBarModule
   ],
-  providers: [],
+  providers: [
+    PersistanceService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
